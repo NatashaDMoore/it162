e1a1147aaab9dc75b2844f7cb7f0b5b508ee65c1
Natasha <natasha.d.moore@seattlecolleges.edu>
2023-01-22 11:58:10 -0800

    css fixes

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
             <img class="headshot" src="images/nmoore.jpg" alt="picture of Natasha">
         <main>
             <h2>About Natasha</h2>
-                <p>I am one of those mythical "locals" who did not transplant to Seattle. I grew up in Kent, WA but have lived in the city for 8 wonderful years. Before seeking a career in tech I worked in management, insurance, and the service industry. Learning to code is very different than anything I've tried before and I'm really enjoying the challenge.</p>
-                <p>When I'm not in front of a computer, I enjoy hiking and camping like any good PNWer. Now that venues are opening up, I'm also looking forward to more live music! Another thing I'm passionate about is coed flag football. I've been on at least one team for almost 5 years. It's a great way to stay active if, like me, you don't make time for the gym.</p>
+                <p>I'm one of those mythical "locals" who didn't transplant to Seattle. I grew up in Kent, WA but have lived in the city for 8 wonderful years. Before seeking a career in tech I worked in management, insurance, and the service industry. Learning to code is very different than anything I've tried before and I'm really enjoying the challenge.</p>
+                <p>Things I enjoy when not in front of a computer: Like any good PNWer, I like hiking, camping, and enjoying a well earned beer. Now that venues are opening up, I'm really excited to see live music again! I'm also passionate about my coed flag football teams. Growing up, I always wanted to play, but there were few opportunities for girls in my town. Signing up for flag was like a dream come true! Now I captain my own team, I support a growing female flag league, and may join a female tackle league next year as a Corner.</p>
         </main>
         </div>
     </div>
