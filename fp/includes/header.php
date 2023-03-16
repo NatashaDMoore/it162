8d0c33b958291bf14658567d0b6d06c37baf6b61
Natasha <natasha.d.moore@seattlecolleges.edu>
2023-03-15 23:57:12 -0700

    nav

--- fp/includes/header.php
+++ fp/includes/header.php
@@ -8,32 +8,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link type="text/css" rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300;1,400&family=Teko:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
 </head>
 
     <body class="<?php echo $body; ?>">
 
-    <h1><?php echo $title; ?></h1>
+    <!-- <h1><?php echo $title; ?></h1> -->
 
       <header>
 
+         <h1 class="logo">
+            <a href="index.php">SEATTLE WOMEN'S<br>FLAG FOOTBALL</a>
+         </h1>
+
+
+
          <!-- ===== Naivgation ===== -->      
+         <input type="checkbox" class="nav-toggle" id="nav-toggle">
+         <nav class="nav" id="myNav">
 
-         <div class="topnav" id="myTopnav">
-                  <?php
-                     echo make_links($nav);
-                  ?>
-               <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-         </div>
+               <div class="nav-links">
 
-         <script>
+                  <?php echo make_links($nav); ?>
+                  <!-- <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a> -->
+
+               </div>
+         </nav>      
+
+         <label for="nav-toggle" class="nav-toggle-label">
+            <span class="icon">&#9776;</span>
+         </label>
+
+         <!-- <script>
             function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
+            var x = document.getElementById("myNav");
+            if (x.className === "nav") {
                x.className += " responsive";
             } else {
-               x.className = "topnav";
+               x.className = "nav";
             }
             }
-         </script>
+         </script> -->
 
       </header>
+
+      <div class="wrapper">
